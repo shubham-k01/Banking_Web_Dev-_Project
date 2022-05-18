7e62f82b04c99fe13e366f803907fad2dc57906a
unknown <skcode123@gmail.com>
2022-05-18 09:47:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Validation from './Components/Validation';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+    <Router>
+    <div style={{backgroundColor:'#f5ccff',height:'50rem'}}>
+    <Navbar/>
+    <Switch>
+      <Route path="/">
+        <Home/>
+      </Route>
+      <Route path="/login">
+        <Login/>
+      </Route>
+      <Route path="/signup">
+        <Validation/>
+      </Route>
+    </Switch>
+    </div>
+    </Router>
+    </>
+  );
+}
+
+export default App;
